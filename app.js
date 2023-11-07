f1d4457758250499f53f2cba0a7c3b332d7416e7
Faiz Shakoor <121912022+fshakoor@users.noreply.github.com>
2023-11-07 00:27:02 -0600

    Added break mode styling and fixed bug clashing with dark mode

--- app.js
+++ app.js
@@ -22,17 +22,33 @@ function changeDisplay() {
   tabTitle.textContent = displayTime.textContent + " - Session";
 }
 
-function setTheme() {
-  const root = document.documentElement;
-  const newTheme = root.className === 'dark' ? 'light' : 'dark';
-  root.className = newTheme;
-}
-document.querySelector('.theme-toggle').addEventListener('click', setTheme)
 const toggleIcon = document.querySelector('.toggle-icon')
 document.querySelector('.theme-toggle').onclick = function() {
   if (toggleIcon.getAttribute("d") === "M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z") {toggleIcon.setAttribute("d","M256 160c-52.9 0-96 43.1-96 96s43.1 96 96 96 96-43.1 96-96-43.1-96-96-96zm246.4 80.5l-94.7-47.3 33.5-100.4c4.5-13.6-8.4-26.5-21.9-21.9l-100.4 33.5-47.4-94.8c-6.4-12.8-24.6-12.8-31 0l-47.3 94.7L92.7 70.8c-13.6-4.5-26.5 8.4-21.9 21.9l33.5 100.4-94.7 47.4c-12.8 6.4-12.8 24.6 0 31l94.7 47.3-33.5 100.5c-4.5 13.6 8.4 26.5 21.9 21.9l100.4-33.5 47.3 94.7c6.4 12.8 24.6 12.8 31 0l47.3-94.7 100.4 33.5c13.6 4.5 26.5-8.4 21.9-21.9l-33.5-100.4 94.7-47.3c13-6.5 13-24.7.2-31.1zm-155.9 106c-49.9 49.9-131.1 49.9-181 0-49.9-49.9-49.9-131.1 0-181 49.9-49.9 131.1-49.9 181 0 49.9 49.9 49.9 131.1 0 181z")}
   else {toggleIcon.setAttribute("d","M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z")}
 };
+
+function darkMode() {
+  const root = document.documentElement;
+  const switchBtn = document.querySelector('#switch')
+  let newTheme;
+  if (root.className === 'break' || root.className === 'light') {newTheme = 'dark'}
+  else if (root.className === 'dark' && switchBtn.textContent == 'BREAK') {newTheme = 'light'}
+  else if (root.className === 'dark' && switchBtn.textContent == 'POMODORO') {newTheme = 'break'}
+  else {newTheme = 'dark'}
+  root.className = newTheme;
+}
+document.querySelector('.theme-toggle').addEventListener('click', darkMode)
+
+function setBreak() {
+  const root = document.documentElement;
+  let newTheme;
+  const switchBtn = document.querySelector('#switch')
+  const status = document.querySelector('.status')
+  if (root.className === 'break') {newTheme = 'light', switchBtn.textContent = 'BREAK', status.textContent = 'CURRENT: POMODORO'} else {newTheme = 'break', switchBtn.textContent = 'POMODORO', status.textContent = 'CURRENT: BREAK'}
+  root.className = newTheme;
+}
+document.querySelector('#switch').addEventListener('click', setBreak)
 
 function setTime(newTime) {
   time = newTime * 60;
